bc9a0c733a1599058605c7a3fe4ff2f4b3f4b8e6
Dmitry Kashitsyn <korvin@deeptown.org>
2013-07-16 23:56:06 +0700

    Adds image builder's header stub

---
+++ include/ib.h
@@ -0,0 +1,37 @@
+#include <memory.h>
+#include <types.h>
+#include <map>
+
+namespace ib {
+    
+struct ImageClass {
+    std::string name;
+    std::string parent;
+    std::vector<std::string> instanceVariables;
+    std::map<std::string, ImageMethod> methods;
+};    
+
+struct ImageMethod {
+    std::string className;
+    std::string name;
+    std::vector<std::string> temporaries;
+    std::vector<std::string> arguments;
+    std::vector<uint8_t> bytecodes;
+};
+
+class ImageBuilder {
+private:
+    std::map<std::string, ImageClass> m_imageObjects;
+    
+public:
+};
+
+class MethodCompiler {
+private:
+    ImageMethod m_currentMethod;
+    
+public:
+    bool compile(const std::string& className, const std::string& methodSource);
+};
+
+}
