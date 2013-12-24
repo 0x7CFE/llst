deaaba6bdcba2a090e3befa09b968a3a81fd1ede
Roman Proskuryakov <humbug@deeptown.org>
2013-12-24 17:05:33 +0400

    Adds more comments
    Issue: #44

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -2,46 +2,18 @@
 #define LINKED_LIST_H_INCLUDED
 
 template <typename T>
-class StackLinkedNode
-{
-typedef StackLinkedNode<T> selfType;
-private:
-    void* operator new(size_t);
-    void operator delete(void*);
-    StackLinkedNode(const selfType&);
-protected:
-    T* m_data;
-    selfType* m_next;
-public:
-    StackLinkedNode() : m_data(0), m_next(0) {}
-    selfType& operator=(const selfType& right) {
-        m_data = right.m_data;
-        m_next = this->m_next; // We copy only data
-        return *this;
-    }
+class StackLinkedNode;
 
-    selfType* setNext(selfType* next) {
-        m_next = next;
-        return this;
-    }
-    selfType* setData(T* data) {
-        m_data = data;
-        return this;
-    }
-    selfType* getNext() {
-        return m_next;
-    }
-    bool hasNext() {
-        return m_next != 0;
-    }
-    T* getData() {
-        return m_data;
-    }
-    T* getData() const {
-        return m_data;
-    }
-};
-
+/* LinkedList is a very fast singly linked list.
+ * It keeps only StackLinkedNode objects, allocated on stack.
+ * Asymptotic complexity:
+ *  insert: O(1)
+ *  erase:
+ *      all but the last: O(1)
+ *      the last element: O(N)
+ *  access:
+ *      head: O(1)
+ */
 template <typename T>
 class LinkedList
 {
@@ -87,6 +59,47 @@ private:
         }
         //something is really wrong;
         return 0;
+    }
+};
+
+template <typename T>
+class StackLinkedNode
+{
+typedef StackLinkedNode<T> selfType;
+private:
+    void* operator new(size_t);
+    void operator delete(void*);
+    StackLinkedNode(const selfType&);
+protected:
+    T* m_data;
+    selfType* m_next;
+public:
+    StackLinkedNode() : m_data(0), m_next(0) {}
+    selfType& operator=(const selfType& right) {
+        m_data = right.m_data;
+        m_next = this->m_next; // We copy only data
+        return *this;
+    }
+
+    selfType* setNext(selfType* next) {
+        m_next = next;
+        return this;
+    }
+    selfType* setData(T* data) {
+        m_data = data;
+        return this;
+    }
+    selfType* getNext() {
+        return m_next;
+    }
+    bool hasNext() {
+        return m_next != 0;
+    }
+    T* getData() {
+        return m_data;
+    }
+    T* getData() const {
+        return m_data;
     }
 };
 
