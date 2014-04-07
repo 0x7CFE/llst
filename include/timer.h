6abfaeb40c1b5058589735957ecf8f294361b84c
vlad <vlastachu@gmail.com>
2014-04-08 01:54:02 +0300

    Completes changes on windows.
    Issue: #47
    
    Conflicts:
    	include/timer.h
    	src/Timer.cpp

--- include/timer.h
+++ include/timer.h
@@ -1,22 +1,23 @@
-#ifdef WINDOWS
-#include <time.h>
-#else
-#include <sys/time.h>
-#endif
 
+#include <string>
+#include <sstream>
+using std::stringstream;
 #ifdef WINDOWS
+	#include <time.h>
     typedef clock_t systemTimeValue;
 #else
+	#include <sys/time.h>
     typedef timeval systemTimeValue;
 #endif
+
 
     
 //analogue of c++11 ratio from chrono.h
 template <int NUM, int DEN>
 struct TRatio
 {
-    const int num = NUM;
-    const int den = DEN;
+    static const int num = NUM;
+    static const int den = DEN;
 };
 
 typedef TRatio<86400,1>      TDay;
@@ -27,45 +28,72 @@ typedef TRatio<1,1000>       TMillisec;
 typedef TRatio<1,1000000>    TMicrosec;
 typedef TRatio<1,1000000000> TNanosec;
 
-//fixed point format 
-//represent nanoseconds
-typedef unsigned long long TTime;
+
+//!тип вне TDuration потому что получается много типов 
+//TDuration<RATIO>::SuffixMode что не логично
+enum SuffixMode {SNONE, SSHORT, SFULL};
 
 //analogue of c++11 duration
-template <class RATIO>
-class TDuraton{
+template <typename RATIO>
+class TDuration{
 private:
-    TTime time;
+	double value;
 public:
-    TDuraton(TTime duration);
-    template <class RATIO2> extern RATIO2 convertTo();
-    char* toString();
-    TTime getValue();
+	//argument: duration in seconds
+	//FIXME should be a private member, but make conflict at convertTo() in such case
+	TDuration(double duration){
+		value = duration;
+	}
+
+	template <typename RATIO2> TDuration<RATIO2> convertTo(){
+		return TDuration<RATIO2>(value * (RATIO::num * RATIO2::den) / (double)(RATIO::den * RATIO2::num));
+	}
+
+	std::string toString(SuffixMode sMode = SuffixMode::SNONE, int symbolsAfterPoint = 0,
+									  const char* pointSymbol = ".", const char* spaceSymbol = " "){
+		stringstream ss; 
+		ss << floor(value);
+		if(symbolsAfterPoint)
+			ss << pointSymbol << floor((value - floor(value)) * pow(10.0, symbolsAfterPoint));
+		if(sMode != SuffixMode::SNONE)
+			ss << spaceSymbol << getSuffix(sMode);
+		return ss.str();
+	}
+	std::string getSuffix(SuffixMode sMode);
+	
+	std::ostream& operator<<(std::ostream& os){
+		os << toString();
+		return os;
+	}
+
+	inline bool operator< (const TDuration<RATIO>& rhs){
+		return value < rhs.value;
+	}
+
+	inline TDuration<RATIO> operator+(const TDuration<RATIO>& rhs){
+		return TDuration<RATIO>(value + rhs.value);
+	}
+
+	inline TDuration<RATIO> operator-(const TDuration<RATIO>& rhs){
+		return TDuration<RATIO>(value - rhs.value);
+	}
+
+	inline bool operator> (const TDuration<RATIO>& rhs){return  !(operator< (rhs));}
+
+	friend class Timer;
 };
-
-template <class RATIO> extern
-std::ostream& operator<<(std::ostream& os, const TDuraton<RATIO>& obj);
-
-template <class RATIO, class RATIO2> extern
-inline bool operator< (const TDuraton<RATIO>& lhs, const TDuraton<RATIO2>& rhs);
-template <class RATIO, class RATIO2> extern
-inline bool operator> (const TDuraton<RATIO>& lhs, const TDuraton<RATIO2>& rhs){return  operator< (rhs,lhs);}
-
-template <class RATIO, class RATIO2> extern
-inline TDuraton<RATIO> operator+(TDuraton<RATIO> lhs, const TDuraton<RATIO2>& rhs);
-template <class RATIO, class RATIO2> extern
-inline TDuraton<RATIO> operator-(TDuraton<RATIO> lhs, const TDuraton<RATIO2>& rhs);
-
 
 
 class Timer{
 private:
-    systemTimeValue timeCreate;
-    TTime timeDiff; 
+    systemTimeValue timeCreate; 
+	double getDiffSec();
 public:
-    Timer();
-    static Timer now();
+	Timer(){}
+	static Timer now() {Timer t; t.start(); return t;}
     void start();
-    Timer* update();
-    TTime getTime();
+	template <typename RATIO>
+	TDuration<RATIO> get(){
+		return TDuration<TSec>(getDiffSec()).convertTo<RATIO>();
+	}
 };
--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,65 +1,54 @@
+//include cmath
 #include "timer.h"
-#include <cmath>
+#include <math.h>
+#include <sstream>
+#define nanosec_in_sec 1000000000
+#define microsec_in_sec 1000000
+using std::stringstream;
 
 #ifdef WINDOWS
-Timer::Timer(){
-    clock_t timeCreate = clock();
+void Timer::start(){
+    timeCreate = clock();
 }
 
-Timer* Timer::update(){
-    float diff = ((float)(clock() - timeCreate))/CLOCKS_PER_SEC;
-    timeDiff.sec = (int)trunc(diff);
-    timeDiff.msec = (int)(diff - timeDiff.sec);
-    return this;
+double Timer::getDiffSec(){
+    return ((double)(clock() - timeCreate))/CLOCKS_PER_SEC;
 }
 #else
-Timer::Timer(){
+void Timer::start(){
     gettimeofday(&timeCreate, 0);
 }
 
-Timer* Timer::update(){
+double Timer::getDiffSec(){
     timeval current;
     gettimeofday(&current, 0);
-    float diff = current.tv_sec + ((float)(current.tv_usec/1000))/1000.f 
-               - (timeCreate.tv_sec + ((float)(timeCreate.tv_usec/1000))/1000.f);
-    timeDiff.sec = (int)trunc(diff);
-    timeDiff.msec = (int)(diff - timeDiff.sec);
-    return this;
+    double diff = current.tv_sec*microsec_in_sec + current.tv_usec/(double)TMicrosec.den
+               - (timeCreate.tv_sec*microsec_in_sec + timeCreate.tv_usec/(double)TMicrosec.den);
+    return diff;
 }
 #endif
 
-TTime Timer::getTime(){
-    return timeDiff;
-}
+
+template <> std::string TDuration<TDay>::getSuffix(SuffixMode sMode){
+	return sMode == SuffixMode::SFULL ? "days" : sMode == SuffixMode::SSHORT ? "d" : ""; }
+
+template <> std::string TDuration<THour>::getSuffix(SuffixMode sMode){
+	return sMode == SuffixMode::SFULL ? "hours" : sMode == SuffixMode::SSHORT ? "h" : ""; }
+
+template <> std::string TDuration<TMin>::getSuffix(SuffixMode sMode){
+	return sMode == SuffixMode::SFULL ? "minutes" : sMode == SuffixMode::SSHORT ? "m" : ""; }
+
+template <> std::string TDuration<TSec>::getSuffix(SuffixMode sMode){
+	return sMode == SuffixMode::SFULL ? "seconds" : sMode == SuffixMode::SSHORT ? "s" : ""; }
+
+template <> std::string TDuration<TMillisec>::getSuffix(SuffixMode sMode){
+	return sMode == SuffixMode::SFULL ? "milliseconds" : sMode == SuffixMode::SSHORT ? "ms" : ""; }
+
+template <> std::string TDuration<TMicrosec>::getSuffix(SuffixMode sMode){
+	return sMode == SuffixMode::SFULL ? "microseconds" : sMode == SuffixMode::SSHORT ? "mcs" : ""; }
+
+template <> std::string TDuration<TNanosec>::getSuffix(SuffixMode sMode){
+	return sMode == SuffixMode::SFULL ? "nanoseconds" : sMode == SuffixMode::SSHORT ? "us" : ""; }
 
 
 
-TDuraton::TDuraton(TTime duration){
-    
-}
-
-template <class RATIO2> RATIO2 convertTo()
-{
-    
-}
-char* toString()
-{
-    
-}
-TTime getValue()
-{
-    
-}
-
-template <class RATIO> extern
-std::ostream& operator<<(std::ostream& os, const TDuraton<RATIO>& obj);
-
-template <class RATIO, class RATIO2> extern
-inline bool operator< (const TDuraton<RATIO>& lhs, const TDuraton<RATIO2>& rhs);
-template <class RATIO, class RATIO2> extern
-inline bool operator> (const TDuraton<RATIO>& lhs, const TDuraton<RATIO2>& rhs){return  operator< (rhs,lhs);}
-
-template <class RATIO, class RATIO2> extern
-inline TDuraton<RATIO> operator+(TDuraton<RATIO> lhs, const TDuraton<RATIO2>& rhs);
-template <class RATIO, class RATIO2> extern
-inline TDuraton<RATIO> operator-(TDuraton<RATIO> lhs, const TDuraton<RATIO2>& rhs);
