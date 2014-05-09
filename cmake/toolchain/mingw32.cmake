a1b62f952ae71677286b047038893c4ba104e648
Roman Proskuryakov <humbug@deeptown.org>
2014-05-09 04:54:12 +0400

    Adds toolchain for mingw32
    Issue: #50

---
+++ cmake/toolchain/mingw32.cmake
@@ -0,0 +1,11 @@
+set (CMAKE_SYSTEM_NAME "Windows")
+
+SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
+SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
+SET(CMAKE_RC_COMPILER i586-mingw32msvc-windres)
+
+# Search headers and libraries in the target environment
+# Search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
