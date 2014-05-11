750cc1fe1952e8b10eabec61b97c41559740afa1
Dmitry Kashitsyn <korvin@deeptown.org>
2014-05-11 19:57:22 +0700

    Rewrites graph visualizator using graphviz
    *nix way rocks!
    
    Issue: #32

--- include/visualization.h
+++ include/visualization.h
@@ -1,37 +1,38 @@
 #ifndef LLST_VISUALIZATION_H_INCLUDED
 #define LLST_VISUALIZATION_H_INCLUDED
 
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
 #include <map>
 #include <analysis.h>
 
-#include <ogdf/basic/Graph.h>
-#include <ogdf/basic/GraphAttributes.h>
-
 class ControlGraphVisualizer : public st::NodeVisitor {
 public:
-    ControlGraphVisualizer(st::ControlGraph* graph) : st::NodeVisitor(graph) { m_ogdfGraph = new ogdf::Graph(); }
-    virtual ~ControlGraphVisualizer() { delete m_ogdfGraph; }
+    ControlGraphVisualizer(st::ControlGraph* graph, const std::string& fileName) : st::NodeVisitor(graph) {
+        m_stream.open(fileName.c_str(), std::ios::out | std::ios::trunc);
+
+        m_stream << "digraph G2 {\n";
+        firstDomain = true;
+    }
+
+    virtual ~ControlGraphVisualizer() { finish(); }
 
     virtual bool visitDomain(st::ControlDomain& domain);
     virtual bool visitNode(st::ControlNode& node);
 
-    void writeGraphTo(const std::string& fileName);
+private:
+    void finish();
+    bool isNodeProcessed(st::ControlNode* node);
+    void markNode(st::ControlNode* node);
 
 private:
-    ogdf::NodeElement* getNodeFor(st::ControlNode*);
-    void addEdge(st::ControlNode* from, st::ControlNode* to);
-    void applyLayout();
-    void setNodeProperties(st::ControlNode* node, ogdf::NodeElement* element);
+    typedef std::map<st::ControlNode*, bool> TNodeMap;
+    TNodeMap m_processedNodes;
 
-private:
-    ogdf::Graph* m_ogdfGraph;
-    ogdf::GraphAttributes* m_ogdfAttributes;
-
-    typedef std::map<st::ControlNode*, ogdf::NodeElement*> TNodeMap;
-    TNodeMap m_nodes;
-
-    uint32_t m_lastX;
-    uint32_t m_lastY;
+    std::ofstream m_stream;
+    bool firstDomain;
 };
 
 #endif
--- src/ControlGraphVisualizer.cpp
+++ src/ControlGraphVisualizer.cpp
@@ -1,150 +1,106 @@
 
 #include <visualization.h>
 
-#include <ogdf/layered/SugiyamaLayout.h>
-#include <ogdf/layered/OptimalRanking.h>
-#include <ogdf/layered/MedianHeuristic.h>
-#include <ogdf/layered/OptimalHierarchyLayout.h>
-
 bool ControlGraphVisualizer::visitDomain(st::ControlDomain& domain) {
-    // TODO Create domain frame
+//     if (!firstDomain)
+//         m_stream << "\t}                         \n" << std::endl; // closing subgraph
 
+    firstDomain = false;
+
+//     m_stream << "\n\tsubgraph cluster_" << domain.getBasicBlock()->getOffset() << " {\n";
     return st::NodeVisitor::visitDomain(domain);
 }
 
-bool ControlGraphVisualizer::visitNode(st::ControlNode& node) {
-    ogdf::NodeElement* element = getNodeFor(&node);
-
-    const st::TNodeSet& inEdges  = node.getInEdges();
-    const st::TNodeSet& outEdges = node.getOutEdges();
-
-    // Processing incoming edges
-    st::TNodeSet::iterator iEdge = inEdges.begin();
-    for (; iEdge != inEdges.end(); ++iEdge)
-        addEdge(*iEdge, &node);
-
-    // Processing outgoing edges
-    iEdge = outEdges.begin();
-    for (; iEdge != outEdges.end(); ++iEdge)
-        addEdge(&node, *iEdge);
-
-    // Processing argument edges
-    if (const st::InstructionNode* instruction = node.cast<st::InstructionNode>()) {
-        for (std::size_t index = 0; index < instruction->getArgumentsCount(); index++) {
-            ogdf::NodeElement* argument = getNodeFor(instruction->getArgument(index));
-            ogdf::EdgeElement* edge     = m_ogdfGraph->newEdge(element, argument);
-
-            m_ogdfAttributes->labelEdge(edge) = index;
-            m_ogdfAttributes->colorEdge(edge) = "blue";
-            m_ogdfAttributes->arrowEdge(edge) = ogdf::GraphAttributes::first;
-        }
-    }
-
-    return st::NodeVisitor::visitNode(node);
-}
-
-void ControlGraphVisualizer::setNodeProperties(st::ControlNode* node, ogdf::NodeElement* element) {
-    // Setting basic attributes
-    m_ogdfAttributes->shapeNode(element) = ogdf::GraphAttributes::oval;
-    m_ogdfAttributes->width(element)  = 32;
-    m_ogdfAttributes->height(element) = 32;
-
-    // Calculating node position
-    uint32_t newX = 0;
-    uint32_t newY = 0;
-
-    // We do not want to apply complex layout because our graph is pretty straighforward.
-    // So placing nodes in natural order but stacking argument nodes where possible.
-    if (node->getInEdges().size()) {
-        // Shifting to the next colon
-        newX = m_lastX + 64;
-        newY = 0;
-    } else {
-        // Stacking it with previous one.
-        newX = m_lastX;
-        newY = m_lastY + 64;
-    }
-
-    m_ogdfAttributes->x(element) = newX;
-    m_ogdfAttributes->y(element) = newY;
-
-    m_lastX = newX;
-    m_lastY = newY;
-
-    // Setting node label
-    ogdf::String& label = m_ogdfAttributes->labelNode(element);
-    switch (node->getNodeType()) {
-        case st::ControlNode::ntPhi:
-            label = "Phi";
-            m_ogdfAttributes->colorNode(element) = "grey";
-            break;
-
-        case st::ControlNode::ntTau:
-            label = "Tau";
-            m_ogdfAttributes->colorNode(element) = "green";
-            break;
-
-        case st::ControlNode::ntInstruction:
-            //label = node.cast<st::InstructionNode>()->getInstruction().toString();
-            m_ogdfAttributes->colorNode(element) = "blue";
-
-        default:
-            label = node->getIndex();
-    }
-}
-
-ogdf::NodeElement* ControlGraphVisualizer::getNodeFor(st::ControlNode* node) {
-    TNodeMap::iterator iNode = m_nodes.find(node);
-
-    if (iNode != m_nodes.end())
-        return iNode->second;
-
-    ogdf::NodeElement* element = m_ogdfGraph->newNode(node->getIndex());
-    setNodeProperties(node, element);
-
-    m_nodes[node] = element;
-    return element;
-}
-
-void ControlGraphVisualizer::addEdge(st::ControlNode* from, st::ControlNode* to) {
-    ogdf::NodeElement* fromElement = getNodeFor(from);
-    ogdf::NodeElement* toElement   = getNodeFor(to);
-
-    ogdf::EdgeElement* edge = m_ogdfGraph->searchEdge(fromElement, toElement);
-    if (edge) // Skipping if edge already exists
-        return;
-
-    edge = m_ogdfGraph->newEdge(fromElement, toElement);
-    m_ogdfAttributes->arrowEdge(edge) = ogdf::GraphAttributes::first;
-
+std::string edgeStyle(st::ControlNode* from, st::ControlNode* to) {
     const st::InstructionNode* fromInstruction = from->cast<st::InstructionNode>();
     const st::InstructionNode* toInstruction   = to->cast<st::InstructionNode>();
 
     if ((fromInstruction && fromInstruction->getInstruction().isBranch()) ||
         (toInstruction && toInstruction->getArgumentsCount() == 0))
     {
-        m_ogdfAttributes->colorEdge(edge) = "grey";
-        m_ogdfAttributes->styleEdge(edge) = ogdf::GraphAttributes::esDash;
+        return "[color=\"grey\" style=\"dashed\"]";
     }
 
-    if (to->getNodeType() == st::ControlNode::ntPhi)
-        m_ogdfAttributes->colorEdge(edge) = "grey";
+    return "";
 }
 
-void ControlGraphVisualizer::applyLayout() {
-    ogdf::SugiyamaLayout SL;
-    SL.setRanking(new ogdf::OptimalRanking);
-    SL.setCrossMin(new ogdf::MedianHeuristic);
+bool ControlGraphVisualizer::visitNode(st::ControlNode& node) {
+    const st::TNodeSet& inEdges  = node.getInEdges();
+    const st::TNodeSet& outEdges = node.getOutEdges();
 
-//     ogdf::OptimalHierarchyLayout* ohl = new ogdf::OptimalHierarchyLayout;
-//     ohl->layerDistance(30.0);
-//     ohl->nodeDistance(25.0);
-//     ohl->weightBalancing(0.8);
-//     SL.setLayout(ohl);
+    // Processing incoming edges
+    st::TNodeSet::iterator iEdge = inEdges.begin();
+    for (; iEdge != inEdges.end(); ++iEdge) {
+        if (isNodeProcessed(*iEdge))
+            continue;
 
-    SL.call(*m_ogdfAttributes);
+        m_stream << "\t\t" << (*iEdge)->getIndex() << " -> " << node.getIndex() << edgeStyle(*iEdge, &node) << ";\n";
+    }
+
+    // Processing outgoing edges
+    iEdge = outEdges.begin();
+    for (; iEdge != outEdges.end(); ++iEdge) {
+        if (isNodeProcessed(*iEdge))
+            continue;
+
+        m_stream << "\t\t" << node.getIndex() << " -> " << (*iEdge)->getIndex() << edgeStyle(&node, *iEdge) << ";\n";
+    }
+
+    // Processing argument edges
+    if (const st::InstructionNode* instruction = node.cast<st::InstructionNode>()) {
+        const std::size_t argsCount = instruction->getArgumentsCount();
+        for (std::size_t index = 0; index < argsCount; index++) {
+            m_stream << "\t\t" << node.getIndex() << " -> " << instruction->getArgument(index)->getIndex() << " [";
+
+            if (argsCount > 1)
+                m_stream << "label=" << index;
+
+            m_stream << " labelfloat=true color=\"blue\" fontcolor=\"blue\" style=\"dashed\" constraint=false];\n";
+        }
+    }
+
+    markNode(&node);
+    return st::NodeVisitor::visitNode(node);
 }
 
-void ControlGraphVisualizer::writeGraphTo(const std::string& fileName) {
-    m_ogdfGraph->writeGML(fileName.c_str());
+bool ControlGraphVisualizer::isNodeProcessed(st::ControlNode* node) {
+    return m_processedNodes.find(node) != m_processedNodes.end();
+}
+
+void ControlGraphVisualizer::markNode(st::ControlNode* node) {
+    // Setting node label
+    std::string label;
+    std::string color;
+
+    switch (node->getNodeType()) {
+        case st::ControlNode::ntPhi:
+            label = "Phi ";
+            color = "grey";
+            break;
+
+        case st::ControlNode::ntTau:
+            label = "Tau ";
+            color = "green";
+            break;
+
+        case st::ControlNode::ntInstruction:
+            //label = node.cast<st::InstructionNode>()->getInstruction().toString();
+            if (node->cast<st::InstructionNode>()->getInstruction().isTerminator())
+                color = "red";
+            break;
+
+        default:
+            ;
+    }
+
+    m_stream << "\t\t" << label << node->getIndex() << " [ color=\"" << color << "\"];\n";
+    m_processedNodes[node] = true;
+}
+
+void ControlGraphVisualizer::finish() {
+//     if (!firstDomain)
+//         m_stream << "\t}                         \n";
+
+    m_stream << "}                         \n";
+    m_stream.close();
 }
