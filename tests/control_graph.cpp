160c972ea5721910f00cb52f5f4b057dd46177a6
Roman Proskuryakov <humbug@deeptown.org>
2014-05-14 21:31:15 +0400

    Adds tests for ControlGraph
    Issue: #32

---
+++ tests/control_graph.cpp
@@ -0,0 +1,174 @@
+#include <gtest/gtest.h>
+#include <instructions.h>
+#include <analysis.h>
+
+class ControlGraphTest : public ::testing::Test
+{
+protected:
+    TMethod* m_method;
+    st::ParsedMethod* m_parsedMethod;
+    st::ControlGraph* m_cfg;
+    virtual void SetUp()
+    {
+        m_method = (new ( calloc(4, sizeof(TMethod)) ) TObject(sizeof(TMethod) / sizeof(TObject*) - 2, 0))->cast<TMethod>();
+        static const uint8_t bytes[] =
+        {
+            // This is the bytecode for method Object>>isKindOf:
+            32,
+            129,
+            144,
+            112,
+            245,
+            193,
+            11,
+            0,
+            48,
+            161,
+            242,
+            48,
+            161,
+            248,
+            38,
+            0,
+            48,
+            33,
+            130,
+            145,
+            248,
+            28,
+            0,
+            91,
+            242,
+            246,
+            29,
+            0,
+            90,
+            245,
+            48,
+            129,
+            146,
+            112,
+            245,
+            246,
+            11,
+            0,
+            80,
+            245,
+            92,
+            242,
+            245,
+            241
+        };
+        TByteObject* bytecode = new ( calloc(4, 4096) ) TByteObject(sizeof(bytes)/sizeof(bytes[0]), static_cast<TClass*>(0));
+        memcpy(bytecode->getBytes(), bytes, bytecode->getSize());
+        m_method->byteCodes = bytecode;
+
+        m_parsedMethod = new st::ParsedMethod(m_method);
+        m_cfg = new st::ControlGraph(m_parsedMethod);
+        m_cfg->buildGraph();
+    }
+    virtual void TearDown() {
+        free(m_method->byteCodes);
+        free(m_method);
+        delete m_cfg;
+        delete m_parsedMethod;
+    }
+};
+
+TEST_F(ControlGraphTest, buildGraphMoreThanOnce)
+{
+    st::ControlGraph cfg(m_parsedMethod);
+    cfg.buildGraph();
+    //cfg.buildGraph();
+    EXPECT_EQ( std::distance(m_cfg->begin(), m_cfg->end()) , std::distance(cfg.begin(), cfg.end()) );
+}
+
+TEST_F(ControlGraphTest, lastInstIsTerminator)
+{
+    class lastIsTerminator: public st::BasicBlockVisitor
+    {
+    public:
+        lastIsTerminator(st::ParsedBytecode* parsedBytecode) : st::BasicBlockVisitor(parsedBytecode) {}
+        virtual bool visitBlock(st::BasicBlock& BB) {
+            std::size_t bbSize = BB.size();
+            EXPECT_NE(bbSize, 0);
+            st::TSmalltalkInstruction terminator(0);
+            bool hasTerminator = BB.getTerminator(terminator);
+
+            {
+                SCOPED_TRACE("Each BB must have a terminator");
+                EXPECT_TRUE(hasTerminator);
+            }
+            {
+                SCOPED_TRACE("The instruction returned by BB::getTerminator must be a terminator");
+                EXPECT_TRUE( terminator.isTerminator() );
+            }
+            {
+                SCOPED_TRACE("The last instruction must be a terminator and it must be equal to BB::getTerminator");
+                st::TSmalltalkInstruction lastInst = BB[bbSize-1];
+                EXPECT_TRUE( lastInst.isTerminator() );
+                EXPECT_EQ( lastInst.serialize(), terminator.serialize() );
+            }
+
+            return true;
+        }
+    };
+    lastIsTerminator visitor(m_cfg->getParsedMethod());
+    visitor.run();
+}
+
+TEST_F(ControlGraphTest, eachDomainHasTerminator)
+{
+    class domainHasTerminator: public st::DomainVisitor
+    {
+    public:
+        TMethod* m_method;
+        domainHasTerminator(st::ControlGraph* graph, TMethod* method) : st::DomainVisitor(graph), m_method(method) {}
+        virtual bool visitDomain(st::ControlDomain& domain) {
+            st::InstructionNode* terminator = domain.getTerminator();
+            {
+                SCOPED_TRACE("Domain must have a terminator");
+                EXPECT_NE( terminator, static_cast<st::InstructionNode*>(0) );
+                EXPECT_TRUE( terminator->getInstruction().isTerminator() );
+            }
+            return true;
+        }
+    };
+    domainHasTerminator visitor(m_cfg, m_method);
+    visitor.run();
+}
+
+TEST_F(ControlGraphTest, BBsAreLinkedTogether)
+{
+    class areBBsLinked: public st::DomainVisitor
+    {
+    public:
+        int& m_numberOfReferrers;
+        areBBsLinked(st::ControlGraph* graph, int& numberOfReferrers) :
+            st::DomainVisitor(graph),
+            m_numberOfReferrers(numberOfReferrers)
+        {}
+        virtual bool visitDomain(st::ControlDomain& domain) {
+            st::BasicBlock::TBasicBlockSet referrers = domain.getBasicBlock()->getReferers();
+            m_numberOfReferrers += referrers.size();
+
+            for(st::BasicBlock::TBasicBlockSet::const_iterator referrer = referrers.begin(); referrer != referrers.end(); ++referrer) {
+                st::TSmalltalkInstruction terminator(0);
+                bool referrerHasTerminator = (*referrer)->getTerminator(terminator);
+
+                EXPECT_TRUE(referrerHasTerminator);
+                EXPECT_TRUE(terminator.isBranch());
+                EXPECT_EQ( terminator.getExtra(), domain.getBasicBlock()->getOffset() );
+            }
+            return true;
+        }
+    };
+    int numberOfReferrers = 0;
+    areBBsLinked visitor(m_cfg, numberOfReferrers);
+    visitor.run();
+
+    {
+        SCOPED_TRACE("There must be at least one referrer");
+        EXPECT_NE(numberOfReferrers, 0);
+    }
+}
