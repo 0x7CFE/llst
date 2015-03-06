e72f452cc7196529045f6fd503ca856b48304590
Roman Proskuryakov <humbug@deeptown.org>
2015-03-06 23:40:42 +0300

    Fixes segfault in stack_semantics
    Issue:  #32

--- tests/stack_semantics.cpp
+++ tests/stack_semantics.cpp
@@ -47,8 +47,9 @@ TEST_P(P_DecodeBytecode, StackSemanticsTemps)
                         SCOPED_TRACE("First arg must be PushTemporary");
                         EXPECT_EQ( firstArg->getNodeType(), st::ControlNode::ntInstruction );
                         st::InstructionNode* pushTemp = firstArg->cast<st::InstructionNode>();
-                        EXPECT_TRUE(pushTemp);
-                        EXPECT_EQ(pushTemp->getInstruction().getOpcode(), opcode::pushTemporary);
+                        EXPECT_TRUE(pushTemp != NULL);
+                        if (pushTemp)
+                            EXPECT_EQ(pushTemp->getInstruction().getOpcode(), opcode::pushTemporary);
                     }
 
                     return false;
