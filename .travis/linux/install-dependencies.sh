fccf2e17e51de1c3764b25f7b4ecc86df146c2a3
Roman Proskuryakov <humbug@deeptown.org>
2015-04-10 11:59:10 +0300

    Do not use ppa to satisfy the deps
    Issue: #65

--- .travis/linux/install-dependencies.sh
+++ .travis/linux/install-dependencies.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 
-sudo apt-get install -y binutils:i386 llvm-3.3-dev:i386 libreadline-dev:i386
+alias apt_install='sudo apt-get -qy --no-install-suggests --no-install-recommends install'
 
-if [ "$CXX" = "clang++" ]; then
-    sudo apt-get install -y g++-4.8-multilib
-fi
-if [ "$CXX" = "g++" ]; then
-    sudo apt-get install -y gcc:i386 g++:i386 cpp:i386 g++-4.6:i386 gcc-4.6:i386
-fi
+apt_install "g++-multilib" gcc-multilib linux-libc-dev:i386 # 32bit compilers
+apt_install libreadline-dev:i386
 
+if [ "$USE_LLVM" = "On" ]; then
+    apt_install llvm-3.3:i386 llvm-3.3-dev:i386 libllvm3.3:i386 llvm-3.3-runtime:i386
+fi
