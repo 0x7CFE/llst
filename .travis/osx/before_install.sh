de82cfd2da56e443ee07f920c1a8fe034e006d17
Roman Proskuryakov <humbug@deeptown.org>
2015-08-11 02:51:50 +0300

    Prepares env and compilers

---
+++ .travis/osx/before_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ "${CC}" == "gcc" ]]; then
+    export CC=gcc-4.8
+    export CXX=g++-4.8
+fi
+
+function brew_upgrade { brew outdated $1 || brew upgrade $1; }
+
+brew update
+brew install readline
